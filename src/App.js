9d7d4858b02cc8c7dedb3a6a2d17e176107ee5a4
pimentellima <matheus_dere@hotmail.com>
2023-04-08 19:29:34 -0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { useState } from 'react'
-import {AnimatePresence, useMotionValue} from 'framer-motion'
-import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom'
-import './App.css';
-import HomePage from './elements/HomePage';
-import NavBar from './elements/NavBar';
-import {motion} from 'framer-motion'
+import React, { useState } from "react";
+import { AnimatePresence, useMotionValue } from "framer-motion";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import "./App.css";
+import HomePage from "./elements/HomePage";
+import NavBar from "./elements/NavBar";
+import { motion } from "framer-motion";
 
 function App() {
-
   const posX = useMotionValue(0);
   const posY = useMotionValue(0);
 
-
   function handleMouse(e) {
-      posX.set(e.pageX);
-      posY.set(e.pageY);
-  };
+    posX.set(e.pageX);
+    posY.set(e.pageY);
+  }
 
   return (
     <div className="App" onMouseMove={handleMouse}>
-      <HomePage mousePosX = {posX} mousePosY={posY} ></HomePage>
+      <HomePage mousePosX={posX} mousePosY={posY}></HomePage>
       <NavBar></NavBar>
     </div>
   );
--- src/elements/HomePage.jsx
+++ src/elements/HomePage.jsx
@@ -1,93 +1,157 @@
-import React, { Component } from 'react'
-import {motion, useAnimate, useMotionValue, useTransform, useSpring ,easeOut, spring} from 'framer-motion'
-import { useState, useEffect, useRef } from 'react';
-import {styled} from "styled-components";
-import Asset1 from './Asset1.svg'
+import React, { Component } from "react";
+import {
+  motion,
+  useAnimate,
+  useMotionValue,
+  useTransform,
+  useSpring,
+  easeOut,
+  spring,
+} from "framer-motion";
+import { useState, useEffect, useRef } from "react";
+import { styled } from "styled-components";
+import Asset1 from "./Asset1.svg";
 
 export default function HomePage(props) {
-//https://framerbook.com/animation/example-animations/26-tracking-the-cursor/
+  //https://framerbook.com/animation/example-animations/26-tracking-the-cursor/
 
+  const windowSizeX = useRef([window.innerWidth, window.innerHeight])
+    .current[0];
+  const windowSizeY = useRef([window.innerWidth, window.innerHeight])
+    .current[1];
 
-  const windowSizeX = useRef([window.innerWidth, window.innerHeight]).current[0]
-  const windowSizeY = useRef([window.innerWidth, window.innerHeight]).current[1]
-
-  const springOptions = {damping:16, stiffness:50, mass:1.5}
-
+  const springOptions = { damping: 16, stiffness: 50, mass: 1.5 };
 
   const [scope, changeOpacity] = useAnimate();
   const [scopeBackground, scale] = useAnimate();
 
   //animation for 'ohayo we are hiro'
 
-  const x = useTransform(props.mousePosX, [0,windowSizeX/2,windowSizeX/2,windowSizeX],[-6, 0, 0, 6]);
-  const y = useTransform(props.mousePosY, [0,windowSizeY/2,windowSizeY/2,windowSizeY],[-6, 0, 0, 6]);
-  const springX = useSpring(x,springOptions);
-  const springY = useSpring(y,springOptions);
+  const x = useTransform(
+    props.mousePosX,
+    [0, windowSizeX / 2, windowSizeX / 2, windowSizeX],
+    [-6, 0, 0, 6]
+  );
+  const y = useTransform(
+    props.mousePosY,
+    [0, windowSizeY / 2, windowSizeY / 2, windowSizeY],
+    [-6, 0, 0, 6]
+  );
+  const springX = useSpring(x, springOptions);
+  const springY = useSpring(y, springOptions);
 
   //animations for 'hiroshi-jap'
-  const backX = useTransform(props.mousePosX, [0,windowSizeX/2,windowSizeX/2,windowSizeX],[-5, 0, 0, 5]);
-  const backY = useTransform(props.mousePosY, [0,windowSizeX/2,windowSizeX/2,windowSizeX],[-5, 0, 0, 5]);
+  const backX = useTransform(
+    props.mousePosX,
+    [0, windowSizeX / 2, windowSizeX / 2, windowSizeX],
+    [-5, 0, 0, 5]
+  );
+  const backY = useTransform(
+    props.mousePosY,
+    [0, windowSizeX / 2, windowSizeX / 2, windowSizeX],
+    [-5, 0, 0, 5]
+  );
   const springBackX = useSpring(backX, springOptions);
-  const springBackY = useSpring(backY,springOptions);
+  const springBackY = useSpring(backY, springOptions);
 
-  const xLeft = useTransform(props.mousePosX, [0,windowSizeX/2,windowSizeX/2,windowSizeX],[13, 0, 0, -13])
-  const yLeft = useTransform(props.mousePosY, [0,windowSizeX/2,windowSizeX/2,windowSizeX],[13, 0, 0, -13])
-  const springXLeft = useSpring(xLeft, springOptions)
-  const springYLeft = useSpring(yLeft, springOptions)
+  const xLeft = useTransform(
+    props.mousePosX,
+    [0, windowSizeX / 2, windowSizeX / 2, windowSizeX],
+    [13, 0, 0, -13]
+  );
+  const yLeft = useTransform(
+    props.mousePosY,
+    [0, windowSizeX / 2, windowSizeX / 2, windowSizeX],
+    [13, 0, 0, -13]
+  );
+  const springXLeft = useSpring(xLeft, springOptions);
+  const springYLeft = useSpring(yLeft, springOptions);
 
-
-
-  
-
-  useEffect(()=>{
-    if(hovering){
-    //animations for .main-text and .ohayo-jap
-      changeOpacity('h1' ,{opacity:0} ,{duration:0.3})
-      changeOpacity('div', {opacity:1} ,{duration:0.3})
+  useEffect(() => {
+    if (hovering) {
+      //animations for .main-text and .ohayo-jap
+      changeOpacity("h1", { opacity: 0 }, { duration: 0.3 });
+      changeOpacity("div", { opacity: 1 }, { duration: 0.3 });
 
       //animations for .jap-text
-      scale(scopeBackground.current, {y:'-50%'})
-      scale(scopeBackground.current, {scale:1.1}, {duration:0.2, ease:'easeOut'})
-    } else{
+      scale(scopeBackground.current, { y: "-50%" });
+      scale(
+        scopeBackground.current,
+        { scale: 1.1 },
+        { duration: 0.2, ease: "easeOut" }
+      );
+    } else {
+      changeOpacity("h1", { opacity: 1 }, { duration: 0.4 });
+      changeOpacity("div", { opacity: 0, y: "-50%" }, { duration: 0.3 });
 
-      changeOpacity('h1', {opacity:1}, {duration:0.4})
-      changeOpacity('div', {opacity:0, y:'-50%'}, {duration:0.3})
-
-
-      scale(scopeBackground.current, {duration:0, y:'-50%'})
-      scale(scopeBackground.current, {scale:1},{duration:0.2, ease:'easeOut'})
+      scale(scopeBackground.current, { duration: 0, y: "-50%" });
+      scale(
+        scopeBackground.current,
+        { scale: 1 },
+        { duration: 0.2, ease: "easeOut" }
+      );
     }
-  })
+  });
 
+  const [hovering, setHovering] = useState(false);
+  return (
+    <div className="homePage">
+      <div className="left-container">
+        <motion.div
+          style={{
+            height: "100%",
+            width: "100%",
+            display: "flex",
 
-
-
-  const [hovering, setHovering] = useState(false)
-    return (
-      <div className='homePage'>
-        <div className='left-container'>
-          <motion.div style = {{height:'100%', width:'100%', display:'flex', 
-
-          x:
-            useSpring(useTransform(props.mousePosX, [0,windowSizeX/2,windowSizeX/2,windowSizeX],[-4, 0, 0, 4]), {stiffness:20, damping:16})
-            }}>
-            <motion.img className='hiro-homePage' src={Asset1}></motion.img>
-          </motion.div>
-        </div>
-        <div className='right-container'>
-          <div className='right-inner-container'>
-            <div ref={scopeBackground} className='back-hiroshi-jap-container'>
-              <motion.div style={{x:springBackX, y:springBackY}} className='jap-text jap'>ヒ</motion.div>
-              <motion.div style={{x:springBackX, y:springBackY}} className='jap-text jap' >ロ</motion.div>
-              <motion.div style={{x:springBackX, y:springBackY}} className='jap-text jap' id='shi-jap'>シ</motion.div>
-            </div>
-            
-            <motion.div style={{x:springX, y:springBackY}} ref ={scope} onMouseEnter = {()=> setHovering(true)} onMouseLeave={()=>setHovering(false)} id='main-text-container' >
-              <h1 className='main-text'>Ohayo, we are Hiro</h1>
-              <div className='jap green ohayo-jap'>オハヨヒロだ</div>
+            x: useSpring(
+              useTransform(
+                props.mousePosX,
+                [0, windowSizeX / 2, windowSizeX / 2, windowSizeX],
+                [-4, 0, 0, 4]
+              ),
+              { stiffness: 20, damping: 16 }
+            ),
+          }}
+        >
+          <motion.img className="hiro-homePage" src={Asset1}></motion.img>
+        </motion.div>
+      </div>
+      <div className="right-container">
+        <div className="right-inner-container">
+          <div ref={scopeBackground} className="back-hiroshi-jap-container">
+            <motion.div
+              style={{ x: springBackX, y: springBackY }}
+              className="jap-text jap"
+            >
+              ヒ
+            </motion.div>
+            <motion.div
+              style={{ x: springBackX, y: springBackY }}
+              className="jap-text jap"
+            >
+              ロ
+            </motion.div>
+            <motion.div
+              style={{ x: springBackX, y: springBackY }}
+              className="jap-text jap"
+              id="shi-jap"
+            >
+              シ
             </motion.div>
           </div>
+
+          <motion.div
+            style={{ x: springX, y: springBackY }}
+            ref={scope}
+            onMouseEnter={() => setHovering(true)}
+            onMouseLeave={() => setHovering(false)}
+            id="main-text-container"
+          >
+            <h1 className="main-text">Ohayo, we are Hiro</h1>
+            <div className="jap green ohayo-jap">オハヨヒロだ</div>
+          </motion.div>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
+}
