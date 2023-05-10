c2ac91c5c647e54784cbd7731a0f9a3e036e481e
luigiloiola <luigi.l.tierno@gmail.com>
2023-05-10 20:09:10 -0300

    aaa

--- src/elements/Projects.jsx
+++ src/elements/Projects.jsx
@@ -1,35 +1,36 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Cards from './Cards'
 import {motion} from 'framer-motion'
-
 //TODO: generate cards automatically
 
 
 function Projects() {
 
-  const title = 'Cabana Burguer appetit appeal'
-  const japTitle = 'オハヨヒロだ'
-  const thumbnail = 'urlToThumbnail'
-  const video = 'urlToVideo'
+
   
   const constraintsRef = useRef(null);
 
+  const [content, setContent] = useState([])
 
+  useEffect(() => {
+    fetch('/cards-content.txt')
+    .then(r => r.text())
+    .then(text => {
+      setContent(text.split("\n"));
+    });
+  }, []);
 
-  
   return (
       <div className='center-container-projects'>
         <div id='recent-projects'>Recent Projects</div>
         <div className='hide-scrollbar' ref={constraintsRef} id='scroll' style={{width:'100vw', height:'100%', overflow:'scroll', position:'relative', marginLeft:'-5vw',}}>
-            <motion.ul className='container-cards green' drag='x' style={{marginRight:'10vw', paddingLeft:'9vw', overflowY:'visible'}} dragConstraints={constraintsRef} dragElastic={0.5}  >
-              <Cards video={video} title={title} japTitle={japTitle} thumbnail = {thumbnail}  />
-              <Cards video={video} title={title} japTitle={japTitle} thumbnail = {thumbnail}  />
-              <Cards video={video} title={title} japTitle={japTitle} thumbnail = {thumbnail}  />
-              <Cards video={video} title={title} japTitle={japTitle} thumbnail = {thumbnail}  />
-              <Cards video={video} title={title} japTitle={japTitle} thumbnail = {thumbnail}  />
+            <motion.ul className='container-cards green' drag='x' style={{marginRight:'10vw', paddingLeft:'9vw', overflowY:'visible'}} dragConstraints={constraintsRef} dragElastic={0.5} >
+              {content.map((line, index)=>{
+                const [title, japTitle, year] = line.split(',');
+                return(<Cards title={title} japTitle={japTitle} year={year} />)
+              })}
             </motion.ul>
         </div>
-
       </div>
   )
 }
