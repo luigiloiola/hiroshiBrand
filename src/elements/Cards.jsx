441c1de145c13fe4d2f7eecdfdb11190dda837d2
luigiloiola <60331397+luigiloiola@users.noreply.github.com>
2023-05-30 15:44:42 -0300

    Update Cards.jsx

--- src/elements/Cards.jsx
+++ src/elements/Cards.jsx
@@ -18,12 +18,12 @@ function Cards(props) {
     if(hovering) {
       //animate1(scope1.current, {} ,{duration:0.3, ease:'easeInOut'},transition)
       //animate2(scope2.current, {height:'83%'},{duration:0.3, ease:'easeInOut'})
-      animate3(scope3.current, {y:'-1.5vh', scale:1.05, width:'48vh', margin:'2.2vh'},transition)
+      //animate3(scope3.current, {y:'-1.5vh', scale:1.05, width:'48vh', margin:'2.2vh'},transition)
       //animate4(scope4.current, {y:'-5.2vh'},transition)
     } else{
       //animate1(scope1.current, {y:0},{duration:0.3, ease:'easeInOut'})
       //animate2(scope2.current, {height:'88%'},{duration:0.3, ease:'easeInOut'})  
-      animate3(scope3.current, {y:0, scale:1,width:'45vh', margin:'1.5vh'},transition)
+      //animate3(scope3.current, {y:0, scale:1,width:'45vh', margin:'1.5vh'},transition)
       //animate4(scope4.current, {y:'-50%'},transition)
     }
     
@@ -33,23 +33,9 @@ function Cards(props) {
 
   return (
     <motion.li className='video-card' ref={scope3} onMouseEnter={()=>{setHovering(true)}} onMouseLeave={()=>{setHovering(false)} }>
-        <div className='thumbnail' style={{height:'68%'}}>
-          <div style={{width:'94%', margin:'3% auto 0 auto',display:'flex',justifyContent:'center', height:'88%', overflow:'hidden', position:'relative'}}> 
-            <img src={png} style={{}}></img>
-          </div>
-          <div  className='subtitle ' style={{display:'grid', height:'12%', gridTemplateColumns:'6fr 1fr', alignItems:'center', position:'relative', transform:'translateY(-3%)'}}>
-            <div className='logo-thumbnail'></div>
-            <span  className='subtitle' style={{color:'black', justifySelf:'end', marginRight:'4%'}}>Lorem ipsum. amet hic quaera</span>
-            <span style={{color:'black'}}>LOGO</span>
-          </div>
-        </div>
-        <div className='card-title'>
-          <div className='title-section' style={{display:'flex', flexDirection:'column', margin:'5%'}}>
-            <div className='title-english'>{props.title}</div>
-            <div className='title-japanise'>{props.japTitle}</div>
-          </div>
-            <span className='card-year'>©{props.year}</span>
-        </div>
+      <div style={{height:'100%', width:'100%'}}>
+        <img src={png} style={{width:'100%'}}></img>
+      </div>
     </motion.li>
     
   )
